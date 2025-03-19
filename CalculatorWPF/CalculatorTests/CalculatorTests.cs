f0e8091489268380ceb88212b2a5a68a5036e096
Tim Heilmann <timtueftel@icloud.com>
2025-03-19 16:53:10 +0100

    new file:   CalculatorWPF/CalculatorTests/CalculatorTests.cs 	new file:   CalculatorWPF/CalculatorTests/CalculatorTests.csproj 	new file:   CalculatorWPF/CalculatorTests/MSTestSettings.cs 	modified:   CalculatorWPF/CalculatorWPF.sln 	modified:   CalculatorWPF/CalculatorWPF/CalculatorWPF.csproj

---
+++ CalculatorWPF/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CalculatorWPF;
+using System.Collections.Generic;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public sealed class CalculatorTests
+    {
+        [TestMethod]
+        public void InfixExpression_GeneratesValidPostfixExpression()
+        {
+            //Arrange
+            List<string> infixExpr = { "100", "X", "99", "-", "3", "/", "57", "+", "0,543" };
+            List<string> expectedPostfixExpr = { "100", "99", "X", "3", "5", "7", "/", "-", "0,543", "+" };
+            Calculator TestCalculator = new Calculator();
+            TestCalculator.InfixExpression = infixExpr;
+
+            //Act
+            TestCalculator.ConvertToPostfix(infixExpr);
+
+            //Assert
+            List<string> actualExpr = TestCalculator._postfixExpr;
+            Assert.AreEqual(expectedPostfixExpr, actualExpr, "Expression did not generate correctly");
+
+
+        }
+    }
+}
