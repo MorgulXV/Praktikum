25f13e56ccc25ea432df0185b0e55fe3cbcebae0
Tim Heilmann <timtueftel@icloud.com>
2025-04-02 11:32:15 +0200

    changed ascii art thing

--- JuPyterNotebooks/file.py
+++
@@ -1,8 +0,0 @@
-def get_submatrix(matrix, start_row, start_col):
-    size = 8
-    return matrix[start_row:start_row + size, start_col:start_col + size]
-
-# Example usage:
-image_matrix = np.array(image)
-submatrix = get_submatrix(image_matrix, 0, 0)
-print(submatrix)
